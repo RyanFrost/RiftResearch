a53a88fb95b1163e048bc4df86971f65308a8c40
Ryan Frost <ryanwindsorfrost@gmail.com>
2014-07-31 15:00:05 -0700

    Treadmill object working

--- RiftGit/ObjectOriented/TreadmillSide/treadmillController.h
+++ RiftGit/ObjectOriented/TreadmillSide/treadmillController.h
@@ -1,22 +1,24 @@
 #include "modbus/modbus.h"
 
+#include <vector>
+
 
 class treadmillController
 {
 private:
 	modbus_t *ctx;
 	
-	std::vector<double> x ( 3, 0.0 );
-	std::vector<double> xFilt ( 3, 0.0 );
-	double aFiltd[3] = {1.0000, -0.9428, 0.3333};
-	double bFiltd[3] = {0.0976, 0.1953, 0.0976};
+	std::vector<double> x;
+	std::vector<double> xFilt;
+	double aFilt[3];
+	double bFilt[3];
 	
 	double uff;
 	double xfoot;
 	double Kd;
-	uint16_t mtp1;
+	uint16_t *mtp1;
 	
-	int initializeModbus(void);
+	// These three are helper functions for moveTreadmill
 	void feedForwardFunction(void);
 	void moveTrack(float);
 	void convert64to16(float);
@@ -25,7 +27,18 @@ public:
 	treadmillController();
 	~treadmillController();
 	
+	// Call this to initialize connection to the linear actuator. Will return 0 if successful,
+	// or -1 if the initialization failed for any reason.
+	int initializeModbus(void);
+	
+	// moveTreadmill takes the arguments (double footPosition, double desiredStiffness) and will
+	// move the treadmill track to achieve the desired stiffness. This is a single action, so it
+	// must be called continuously in a while loop with updated foot position to move the track
+	// for a duration of time.
 	void moveTreadmill(double, double);
 	
+	// infStiffness automatically sets the track position to give infinite stiffness. Only needs
+	// to be called once to set.
+	void infStiffness(void);
 	
 };
