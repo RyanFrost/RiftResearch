e241d1894f6c972cfd3e50adb548d999ff4a213c
RyanDesktopLinux <ryanwindsorfrost@gmail.com>
2015-05-06 18:41:39 -0700

    Stride length analysis

--- RiftGit/ObjectOriented/TreadmillSide/Analysis/For _Ryan/analysis.R
+++ RiftGit/ObjectOriented/TreadmillSide/Analysis/For _Ryan/analysis.R
@@ -1,52 +1,83 @@
-dat[,time:=time_vst_absolute-dat$time_vst_absolute[1]]
 
-heelstrike <- c(0,diff(dat$movingForward))
+if( !exists("gaitData") || !is.data.table(gaitData))
+{
+  source("readAndCleanData.R")
+}
+
+gaitData[,time:=time_vst_absolute-gaitData$time_vst_absolute[1]]
+
+heelstrike <- c(0,diff(gaitData$movingForward))
 heelstrike[heelstrike==1] <- 0
 
-dat[,cycle:=cumsum(-heelstrike)]
+gaitData[,cycle:=cumsum(-heelstrike)]
 
-dat[,perturb:= as.integer(median(perturb)),by=cycle]
-dat2<-dat
-dat2[,time:=time-min(time),by=cycle]
-dat2 <- dat2[hip_right < 1000]
+gaitData[,perturb:= as.integer(median(perturb)),by=cycle]
 
-nSpline <- 100
+gaitData[,time:=time-min(time),by=cycle] # make time start from beginning of cycle for each cyle
 
 
-myfunc <- function(x,y,sequence)
+gaitData[hip_right   > 1000, hip_right    := NA]
+gaitData[hip_left    > 1000, hip_left     := NA]
+gaitData[ankle_right <  -50, ankle_right  := NA]
+gaitData[ankle_left  <  -50, ankle_left   := NA]
+gaitData[knee_right  > 1000, knee_right   := NA]
+gaitData[knee_left   > 1000, knee_left    := NA]
+
+## This function fits data to a spline with the desired number of points. It is
+## used to upsample the kinematic data from each gait cycle to a uniform number 
+## of equally spaced points so that cycles may be compared to one another
+
+makeSpline <- function(x,y,sequence)
 {
-    
-    y=spline(x=x,y=y,xout=sequence,method="natural")$y
-    list(y=y,x=sequence)
+  y=spline(x=x,y=y,xout=sequence,method="natural")$y
+  list(y=y,x=sequence)
 }
 
 
 
-splines <- dat2[tspeed_desired==700,
-                list(cycle,
-                     perturb,
-                     time,
-                     xf,
-                     xfr,
-                     hip_right,
-                     knee_right,
-                     ankle_right,
-                     hip_left,
-                     knee_left,
-                     ankle_left)][cycle > 60,
-                                  myfunc(time,
-                                         xfr,
-                                         seq(0,max(time),length.out=nSpline)),
-                                  by=list(cycle,perturb)]
+nSpline <- 50  # Number of points to fit each spline to
+
+upToSpeed <- gaitData[tspeed_desired==700,
+                      list(cycle,
+                           perturb,
+                           time,
+                           xf,
+                           xfr,
+                           hip_right,
+                           knee_right,
+                           ankle_right,
+                           hip_left,
+                           knee_left,
+                           ankle_left)]
+
+
+splines <- upToSpeed[cycle > 60 & cycle < 1003,
+                     makeSpline(time,
+                                xf,
+                                seq(0,max(time),length.out=nSpline)),
+                     by=list(cycle,perturb)]
+
+
 splines[,pgc:=seq(0,100,length.out=nSpline)]
 
+postPertCycs <- unique(splines[perturb > 0,list(cyc=cycle+1,pert=perturb)])
+splines[cycle %in% postPertCycs[,cyc],perturb := postPertCycs[cyc==cycle,pert] +3 ]
 
 
 
-avgs <- splines[,list(mn=mean(y),std=sd(y),sse=sum((y-mean(y))^2)),by=list(pgc,perturb)]
+avgs <- splines[,
+                list(mn=mean(y),
+                      std=sd(y),
+                      sse=sum((y-mean(y))^2)),
+                by=list(pgc,perturb)]
 
-xfmax <- dat[,max(xf),by=perturb]
 
+## Statistical Analysis
+
+
+
+
+# Get number of cycles for each type of perturbation
 nSamples <- table(splines$perturb)/nSpline
 n0 <- unname(nSamples[names(nSamples)=='0'])
 n1 <- unname(nSamples[names(nSamples)=='1'])
@@ -55,11 +86,16 @@ n3 <- unname(nSamples[names(nSamples)=='3'])
 
 
 
-nh02 <- 2/(1/n0+1/n2)
+nh02 <- 2/(1/n0+1/n2)    # Harmonic mean of sample sizes for pert types 0 and 2
 df02 <- (n0+n2)/2
 
-tmp <- avgs[perturb %in% c(0,2)][,dm:=diff(mn),by=pgc][,s:=sqrt(2*sum(sse)/df02/nh02),by=pgc]
-diffStd_0_2 <- tmp[,list(pgc,dm,s)]
+diffStd_0_2 <- avgs[perturb %in% c(0,2),
+            c("dm","s"):=list( diff(mn) , sqrt(2*sum(sse)/df02/nh02) ),
+            by=pgc]
+
+
+
+diffStd_0_2 <- diffStd_0_2[,list(pgc,dm,s)]
 setkey(diffStd_0_2,s)
 diffStd_0_2 <- unique(diffStd_0_2)
 
@@ -81,15 +117,35 @@ mins2 <- mins[,list(lowmn=mean(lowpt),lowsd=sd(lowpt)),by=list(perturb)]
 maxs <- splines[,list(highpt=max(y)),by=list(perturb,cycle)]
 maxs2 <- maxs[,list(highmn=mean(highpt),highsd=sd(highpt)),by=list(perturb)]
 
-extrema <- splines[,list(low=min(y),high=max(y)),by=list(perturb,cycle)] %>%
+extrema <- splines[,list(time=max(x),low=min(y),high=max(y)),by=list(perturb,cycle)] %>%
   gather(extreme,value,high,low)
-extrema2 <- extrema[,list(mn=mean(value),std=sd(value)),by=list(perturb,extreme)]
+extremaMeans <- extrema[,list(mnTime=mean(time),sdTime=sd(time),mn=mean(value),std=sd(value)),by=list(perturb,extreme)]
 
+
+strideLength <- splines[pgc %in% c(0,100),list(time=max(x),val=y),by=list(perturb,cycle,point=pgc/100)]
+strideLength1 <- strideLength[,list(time=max(time),diffVal=diff(val)),by=list(perturb,cycle)]
+setkey(strideLength1,perturb)
+strideLength2 <- strideLength1[,list(numSamples=.N,mnTime=mean(time),sdTime=sd(time),mnVal=mean(diffVal),sdVal=sd(diffVal)),by=list(perturb)]
+
+strideLength2[,SL:=mnTime*70+mnVal]
+strideLength2[,sdSL:=sqrt((70*sdTime)^2+sdVal^2)]
+
+setkey(strideLength2,perturb)
+
+print(strideLength2)
+
+#strideLength3[,strideLength:=]
 #fit <- lm(value~factor(extreme)+factor(perturb)+cycle,extrema[perturb%in%c(0,1,2)])
 fit <- lm(lowpt~factor(perturb)+(cycle),mins[perturb%in%c(0,2)])
 
-print(anova(fit))
-print(summary(fit))
+
+
+
+t <- t.test(strideLength1[perturb==0,diffVal],strideLength1[perturb==5,diffVal])
+
+
+print(t)
+
 p <- ggplot(avgs[perturb %in% c(0,1,2)],aes(x=pgc,y=mn,colour=factor(perturb))) +
     geom_line() +
     geom_ribbon(aes(ymin=mn-std,ymax=mn+std,fill=factor(perturb)),alpha=0.3,colour=NA)
@@ -115,7 +171,18 @@ p7 <- ggplot(extrema,aes(x=cycle,y=value,colour=factor(perturb)))+
   geom_point() +
   stat_smooth(aes(fill=factor(perturb)),method="lm") +
   facet_grid(extreme~.,scales="free_y")
+
+p8 <- ggplot(strideLength1,aes(x=diffVal)) +
+  geom_density(aes(fill=factor(perturb)),alpha=0.3)
+
+p9 <- ggplot(strideLength1,aes(x=cycle,y=diffVal,colour=factor(perturb))) +
+  geom_point(size=3) +
+  geom_smooth(aes(fill=factor(perturb)),method="lm")
+
 print(p)
 print(p3)
 print(p5)
 print(p6)
+print(p7)
+print(p8)
+print(p9)
--- RiftGit/ObjectOriented/TreadmillSide/Analysis/For _Ryan/readAndCleanData.R
+++ RiftGit/ObjectOriented/TreadmillSide/Analysis/For _Ryan/readAndCleanData.R
@@ -5,9 +5,9 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-dat <- fread("erinData3_22_15.txt")
-dat[,V54:=NULL]
-setnames(dat, c(
+gaitData <- fread("erinData3_22_15.txt")
+gaitData[,V54:=NULL]
+setnames(gaitData, c(
   "motorv_int",
   "lcR_lbs",
   "lcL_lbs",
@@ -62,24 +62,20 @@ setnames(dat, c(
   "marker_y_r_5",
   "marker_y_r_6"))
 
-dat[,motorv_int:=NULL]
-dat[,numEMG := NULL]
-dat[,xd_raw := NULL]
-dat[,angle_enc:=NULL]
-dat[,Fb_raw:=NULL]
-dat[,xact:=NULL]
-dat[,Kd:=NULL]
-dat[,Kact:=NULL]
-dat[,elapsed:=NULL]
-dat[,distance:=NULL]
 
-dat3 <- dat %>% gather(jointleg,angle,c(hip_left,
-                                   knee_left,
-                                   ankle_left,
-                                   hip_right,
-                                   knee_right,
-                                   ankle_right)) %>%
-  separate(jointleg,c("joint","leg"),sep="_")
+# Remove unneeded columns
 
-dat[,xfr:=(marker_y_r_5+marker_y_r_6)/2]
+gaitData[,motorv_int:=NULL]
+gaitData[,numEMG := NULL]
+gaitData[,xd_raw := NULL]
+gaitData[,angle_enc:=NULL]
+gaitData[,Fb_raw:=NULL]
+gaitData[,xact:=NULL]
+gaitData[,Kd:=NULL]
+gaitData[,Kact:=NULL]
+gaitData[,elapsed:=NULL]
+gaitData[,distance:=NULL]
+
+# Add column for right foot position
+gaitData[,xfr:=(marker_x_r_5+marker_x_r_6)/2]
 
